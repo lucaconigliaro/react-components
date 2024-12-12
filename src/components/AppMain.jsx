5e53b0b514163e2d72afbd5d06e81b834a03f6a8
lucaconigliaro <lucaconigliaro1@gmail.com>
2024-12-12 14:12:20 +0100

    feat: main complete

---
+++ src/components/AppMain.jsx
@@ -0,0 +1,17 @@
+import BlogCard from "./BlogCard"
+
+function AppMain() {
+    return (
+        <main>
+            <div className="container">
+                <div className="row">
+                    <div className="col">
+                        <BlogCard />
+                    </div>
+                </div>
+            </div>
+        </main>
+    )
+}
+
+export default AppMain
---
+++ src/components/ButtonCard.jsx
@@ -0,0 +1,11 @@
+import style from "./ButtonCard.module.css"
+
+function ButtonCard() {
+    return (
+        <button className={style.button}>
+            LEGGI DI PIU
+        </button>
+    )
+}
+
+export default ButtonCard
